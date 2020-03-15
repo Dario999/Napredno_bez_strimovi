87b42002ebd6d9dd50ebe44345ac16bfff7c13b7
Dario999 <darioivanovski33@gmail.com>
2020-03-15 14:45:03 +0100

    streamtest1

--- src/Streams/StreamTest1.java
+++ src/Streams/StreamTest1.java
@@ -1,22 +1,30 @@
 package Streams;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.OptionalInt;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class StreamTest1 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        int[] numbers = {10,22,3,45,1};
+        int[] numbers = {10,22,3,45,1,22};
 
 
         IntStream
                 .of(numbers)
                 .distinct()
-                .sorted()
-                .limit(3)
                 .forEach(x -> System.out.println(x));
+
+
+
 
     }
 
